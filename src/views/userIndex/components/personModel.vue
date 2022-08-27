ed193cd379ae10a02dce1720273645263b219ece
快乐每一天 <824533705@qq.com>
2022-08-27 20:52:48 +0800

    图片上传已好

---
+++ src/views/userIndex/components/personModel.vue
@@ -0,0 +1,187 @@
+<template>
+  <el-dialog
+    title="人员"
+    :visible.sync="personVisible"
+  >
+    <el-form :model="personForm" label-width="100px" :rules="personRules">
+      <el-form-item label="人员名称" prop="userName" label-width="100px">
+        <el-input v-model="personForm.userName" maxlength="5" show-word-limit placeholder="请输入" />
+      </el-form-item>
+      <el-form-item label="角色" prop="roleId">
+        <el-select v-model="personForm.roleId" clearable placeholder="请选择">
+          <el-option v-for="item in roleList" :key="item.roleId" :label="item.roleName" :value="item.roleId" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="联系电话" prop="mobile">
+        <el-input v-model="personForm.mobile" maxlength="11" show-word-limit placeholder="请输入" />
+      </el-form-item>
+      <el-form-item label="负责区域" prop="regionId">
+        <el-select v-model="personForm.regionId" placeholder="请选择" @change="addRegionName">
+          <el-option v-for="item in regionList" :key="item.id" :label="item.name" :value="item.id" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="头像" prop="image">
+        <el-upload
+          class="avatar-uploader"
+          action="`${process.env.NODE_ENV}/api/vm-service/sku/fileUpload`"
+          :show-file-list="false"
+          :http-request="uploadFile"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+          :headers="headerObj"
+        >
+          <!-- action="http://likede2-admin.itheima.net/likede/api/vm-service/sku/fileUpload" -->
+          <img v-if="personForm.image" :src="personForm.image" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon" />
+        </el-upload>
+
+      </el-form-item>
+      <el-form-item label="状态" prop="status">
+        <el-checkbox v-model="personForm.status" label="是否启用" name="type" />
+      </el-form-item>
+
+      <el-form-item size="large">
+        <el-button type="primary">立即创建</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+
+    </el-form>
+
+  </el-dialog>
+</template>
+
+<script>
+import { userServiceRole, vmServiceGet, fileUpload } from '@/api/userIndex'
+export default {
+  props: {
+    personVisible: {
+      type: Boolean,
+      default: () => {
+        return true
+      }
+    }
+  },
+  data() {
+    const token = this.$store.getters.token
+    return {
+      personForm: {
+        userName: '',
+        roleId: '',
+        mobile: '',
+        regionId: '',
+        regionName: '',
+        image: '',
+        status: false
+      },
+      personRules: {
+        userName: [
+          { required: true, message: '请输入人员名称', trigger: 'blur' },
+          { max: 5, message: '长度在不超过5个字符', trigger: 'blur' }
+        ],
+        roleId: [
+          { required: true, message: '请选择角色', trigger: 'change' }
+        ],
+        mobile: [
+          { required: true, message: '请填联系电话', trigger: 'blur' },
+          { max: 11, message: '长度在不超过11个字符', trigger: 'blur' }
+        ],
+        regionId: [
+          { required: true, message: '请填选择区域', trigger: 'change' }
+        ],
+        image: [
+          { required: true, message: '请上传图像', trigger: 'blur' }
+        ]
+      },
+      roleList: [],
+      regionData: {
+        pageIndex: 1,
+        pageSize: 1000
+      },
+      regionList: [],
+      headerObj: {
+        Authorization: token
+      }
+    }
+  },
+  created() {
+    this.userServiceRole()
+    this.vmServiceGet()
+  },
+  methods: {
+    async userServiceRole() {
+      const { data } = await userServiceRole()
+      this.roleList = data
+    //   console.log('this.roleList', this.roleList)
+    },
+    async vmServiceGet() {
+      const { data } = await vmServiceGet()
+      this.regionList = data.currentPageRecords
+      console.log('this.regionList', this.regionList)
+    },
+    addRegionName() {
+      console.log(this.personForm.regionId)
+      const regionItem = this.regionList.filter((item) => {
+        return this.personForm.regionId === item.id
+      })
+      this.personForm.regionName = regionItem[0].name
+      console.log('regionItem', regionItem)
+    },
+    handleAvatarSuccess(res, file) {
+    },
+    beforeAvatarUpload(file) {
+      console.log(999999)
+      const types = ['image/jpeg', 'image/jpg']
+      const isImage = types.includes(file.type)
+      console.log('isImage', isImage)
+      if (!isImage) {
+        this.$message.error('上传图片只能是 JPG、JPEG!')
+        return false
+      }
+      const isLtSize = file.size / 1024 / 1024 < 1
+      if (!isLtSize) {
+        this.$message.error('上传图片大小不能超过 1MB!')
+        return false
+      }
+      return true
+    },
+    // 上传图片
+    async uploadFile(params) {
+      const _file = params.file
+      const formData = new FormData()
+      formData.append('fileName', _file)
+      const { data } = await fileUpload(formData)
+      console.log('data1111', data)
+      this.personForm.image = data
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+::v-deep .el-form-item__content{
+    margin-bottom: 20px;
+}
+::v-deep  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 90px;
+    height: 90px;
+    line-height: 90px;
+    text-align: center;
+  }
+  .avatar {
+    width: 90px;
+    height: 90px;
+    display: block;
+  }
+</style>
--- src/views/userIndex/index.vue
+++ src/views/userIndex/index.vue
@@ -1,10 +1,108 @@
 <template>
-  <div class="dashboard-container">人员列表</div>
+  <div class="dashboard-container">
+    <el-card style="margin-bottom:20px">
+      <el-form ref="form" :model="formData" label-width="80px">
+        <el-row :gutter="20">
+          <el-col :span="6">
+            <el-form-item label="人员搜索">
+              <el-input v-model="formData.userName" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-button type="primary" icon="el-icon-search" @click="search">查询</el-button>
+          </el-col>
+        </el-row>
+
+      </el-form>
+    </el-card>
+    <el-card>
+      <myTable
+        :table-date="tableDate"
+        :thead="thead"
+        :total-count="totalCount"
+        :page-index="formData.pageIndex"
+        :total-page="totalPage"
+        @click="click"
+      >
+        <template v-slot:action="{row}">
+          <el-button type="text">修改</el-button>
+          <el-button type="text">删除</el-button>
+        </template>
+      </myTable>
+    </el-card>
+    <personModel />
+  </div>
 </template>
 
 <script>
+import myTable from '@/components/Table'
+import { userServiceList } from '@/api/userIndex'
+import personModel from './components/personModel.vue'
 export default {
-
+  name: 'UserIndex',
+  components: { myTable, personModel },
+  data() {
+    return {
+      formData: {
+        pageIndex: 1,
+        pageSize: 10,
+        userName: ''
+      },
+      tableDate: [],
+      totalCount: '',
+      totalPage: '',
+      thead: [
+        {
+          prop: 'userName',
+          label: '人员名称'
+        },
+        {
+          prop: 'regionName',
+          label: '归属区域'
+        },
+        {
+          prop: 'roleName',
+          label: '角色'
+        },
+        {
+          prop: 'mobile',
+          label: '联系电话'
+        },
+        {
+          label: '操作',
+          prop: 'action',
+          slotName: 'action'
+        }
+      ]
+    }
+  },
+  created() {
+    this.userServiceList()
+  },
+  methods: {
+    async userServiceList() {
+      const { data } = await userServiceList(this.formData)
+      this.tableDate = data.currentPageRecords.map(item => {
+        item.roleName = item.role.roleName
+        return item
+      })
+      console.log('this.tableDate', this.tableDate)
+      this.totalCount = data.totalCount
+      this.totalPage = data.totalPage
+    },
+    click(val) {
+      if (val === 0) {
+        this.$set(this.formData, 'pageIndex', Number(this.formData.pageIndex) - 1)
+      } else if (val === 1) {
+        this.$set(this.formData, 'pageIndex', Number(this.formData.pageIndex) + 1)
+      }
+      this.userServiceList()
+    },
+    search() {
+      this.$set(this.formData, 'pageIndex', 1)
+      this.userServiceList()
+    }
+  }
 }
 </script>
 
